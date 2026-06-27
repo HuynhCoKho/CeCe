237fd5c48ab8964c41cd4c041b18a736d224973a
Cỏ Khô <92889822+HuynhCoKho@users.noreply.github.com>
2026-06-27 12:32:50 +0700

    Connect CeCe to Google Sheet stats

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.CECE_GAME_CONFIG = {
-  STATS_WEB_APP_URL: ''
+  STATS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxT3cnUtV2qWJyKNEDEdk68Z_zvsD4hgBDh11KivHzXCsd2ITLIbE3EfgDUBDKl7GHL/exec'
 };
